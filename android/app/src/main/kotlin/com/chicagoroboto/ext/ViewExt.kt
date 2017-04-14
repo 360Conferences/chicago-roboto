372b8ba61e94148decc023a269bedfb58b931de0
Jerrell Mardis <jmardis@salesforce.com>
2017-04-14 14:33:54 -0500

    Finds the correct Context

--- android/app/src/main/kotlin/com/chicagoroboto/ext/ViewExt.kt
+++ android/app/src/main/kotlin/com/chicagoroboto/ext/ViewExt.kt
@@ -1,6 +1,17 @@
 package com.chicagoroboto.ext
 
 import android.app.Activity
+import android.content.ContextWrapper
 import android.view.View
 
-fun View.getActivity(): Activity? = if (context is Activity) context as Activity? else null
+fun View.getActivity(): Activity? {
+  var context = context
+  while (context !is Activity) {
+    if (context is ContextWrapper) {
+      context = context.baseContext
+    } else {
+      context = null
+    }
+  }
+  return context
+}
